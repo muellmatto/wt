5a3eda635b82ad9338cefd09eb3998e4d5bd1e40
matto <matto@damniam.de>
2017-08-10 19:07:14 +0200

    forgot row and section wrap

--- php/home.php
+++ php/home.php
@@ -1,28 +1,28 @@
-<?php
-    $totalPublishedPosts = $dbPosts->numberPost(true);
-    $posts = buildPostsForPage(0, $totalPublishedPosts, true, false);
-    foreach ($posts as $Post):
-?>
-
-
-<a href="<?php echo $Post->permalink() ?>">
-    <div class="col-1-3">
-        <div class="feed-card">
-            <img src="https://unsplash.it/1000/800/?image=731" alt="?image=731">
-                <h3>
-                    <?php echo $Post->title() ?>
-                </h3>
-                <p>
-                    <?php echo $Post->content(false) ?>
-                </p>
+<div class="row">
+    <div class="section">
+        <?php
+            $totalPublishedPosts = $dbPosts->numberPost(true);
+            $posts = buildPostsForPage(0, $totalPublishedPosts, true, false);
+            foreach ($posts as $Post):
+        ?>
+        <div class="col-1-3">
+            <div class="feed-card">
+                <img src="<?php echo $Post->coverImage() ?>">
+                <a href="<?php echo $Post->permalink() ?>">
+                    <h3>
+                        <?php echo $Post->title() ?>
+                    </h3>
+                </a>
+                    <p>
+                        <?php echo $Post->description(); ?>
+                    </p>
+            </div>
         </div>
+        <?php
+            endforeach; 
+        ?>
     </div>
-</a>
-
-
-<?php
-    endforeach; 
-?>
+</div>
 
 
 
