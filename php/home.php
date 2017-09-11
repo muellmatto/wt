3954e7e1024bd4d3074c10b4ae8b21fe9ea2c3e8
matto <matto@damniam.de>
2017-09-11 14:11:58 +0200

    pagination

--- php/home.php
+++ php/home.php
@@ -6,8 +6,8 @@
             </div>
         </div>
         <?php
-            $totalPublishedPosts = $dbPosts->numberPost(true);
-            $posts = buildPostsForPage(0, $totalPublishedPosts, true, false);
+            // $totalPublishedPosts = $dbPosts->numberPost(true);
+            // $posts = buildPostsForPage(0, $totalPublishedPosts, true, false);
             foreach ($posts as $Post):
         ?>
         <div class="col-1-3">
@@ -29,5 +29,23 @@
     </div>
 </div>
 
+<div class="row">
+    <div class="section">
+        <!-- Pagination -->
+        <div class="pagination">
+            <?php
+                if( Paginator::get('showNewer') ) {
+                    // echo '<li><a href="'.Paginator::urlFirstPage().'" class="btn small">'.$Language->get('First page').'</a></li>';
+                    echo '<a href="'.Paginator::urlPrevPage().'" class="previous_page" style="float: left;">← '.$Language->get('Prev page').'</a>';
+                }
+
+                if( Paginator::get('showOlder') ) {
+                    echo '<a href="'.Paginator::urlNextPage().'" class="next_page" style="float: right;">'.$Language->get('Next page').' →</a>';
+                    // echo '<li><a href="'.Paginator::urlLastPage().'" class="btn small">'.$Language->get('Last page').'</a></li>';
+                }
+            ?>
+        </div>
+    </div>
+</div>
 
 
